ddc903471ccd817cbee089e9b200b213574b4b13
Diego Ubirajara <diego.ubirajara@mail.com>
2017-02-12 11:39:23 +0100

    add generator example

--- generators.py
+++ generators.py
@@ -10,3 +10,15 @@ next(gen) # return 6
 next(gen) # return 12
 next(gen) # return 36
 
+
+#Generators expression
+gex = (n for n in range(10))
+
+next(gex) # return 0
+next(gex) # return 1
+
+gen = (n * n for n in range(5, 9))
+
+next(gen) # return 25
+next(gen) # return 36
+next(gen) # return 49
