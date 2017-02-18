740e53e0405be16681cffb0ac912563f87ea18f7
Diego Ubirajara <diego.ubirajara@mail.com>
2017-02-18 18:59:10 +0100

    update

--- imdb.py
+++ imdb.py
@@ -4,10 +4,10 @@ import json
 
 movie = input('Search the movie: ')
 
-url = f"http://www.omdbapi.com/?t={movie}&y=&plot=full"
+url = 'http://www.omdbapi.com/?t={movie}&y=&plot=full&r=json'
+
 response = requests.get(url)
 values = json.loads(response.text)
 
 for i, y in values.items():
-    print(i,':', y)
-
+    print(i, ':', y)
