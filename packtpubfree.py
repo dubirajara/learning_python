d92e65d2000cf6f9edc181f10262b6393efb1a77
Diego Ubirajara <diego.ubirajara@mail.com>
2017-05-20 19:33:31 +0200

    add crawler packtpubfree scripty

---
+++ packtpubfree.py
@@ -0,0 +1,52 @@
+import re
+from bs4 import BeautifulSoup
+import requests
+# from datetime import datetime
+# from threading import Timer
+
+
+url = 'https://www.packtpub.com/packt/offers/free-learning'
+
+header = {}
+header['Host'] = 'www.packtpub.com'
+header['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
+header['Accept'] = '*/*'
+header['Accept-Language'] = 'en-US,en;q=0.5'
+header['Accept-Encoding'] = 'gzip, deflate'
+header['Upgrade-Insecure-Requests'] = '1'
+
+r = requests.get(url, headers=header)
+soup = BeautifulSoup(r.text, 'lxml')
+
+
+words = soup.find_all(text=re.compile('Python'))
+
+
+def get_packpub():
+    if words:
+        thumbnails = soup.select('div.dotd-main-book-image.float-left img')
+        thumb = 'http:' + thumbnails[0].get('src')
+        print(thumb)
+        print(soup.h2.get_text().strip())
+
+        word = [word.strip() for word in words]
+
+        print(word[2])
+
+    else:
+        print('no match')
+#     cron_task()
+
+
+# def cron_task():
+
+#     dt1 = datetime.today()
+#     dt2 = dt1.replace(day=dt1.day+1, hour=13, minute=27, second=0, microsecond=0)
+#     delta_t = dt2 - dt1
+#     secs = delta_t.seconds + 1
+
+#     task = Timer(secs, get_packpub)
+#     task.start()
+
+
+get_packpub()
