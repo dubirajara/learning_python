6936c9239713016b8fb67964b94642be2f1a113a
Diego Ubirajara <diego.ubirajara@mail.com>
2018-12-19 18:32:38 +0100

    add string n string problem solved

---
+++ string_n_string.py
@@ -0,0 +1,19 @@
+"""
+We have a string s
+We have a number n
+
+Create a function string_n_string(s, n) that takes your string, concatenates the even-indexed chars to the front, 
+odd-indexed chars to the back and return the result of the string after applying the function to it n times.
+"""
+
+def string_n_string(s, n):
+    for _ in range(n):
+        even = [char for i, char in enumerate(s) if i % 2 == 0]
+        odd = [char for i, char in enumerate(s) if i % 2 != 0]
+        s = ''.join(even + odd)
+
+    return s
+
+
+assert string_n_string("Wow Example!", 1) == "WwEapeo xml!" #testcase
+assert string_n_string("qwertyuio", 2) == "qtorieuwy" #testcase
