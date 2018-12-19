6b9d3e8714b37edc2c088a36ea204e1bfaf6ac62
Diego Ubirajara <diego.ubirajara@mail.com>
2018-12-19 18:35:20 +0100

    add length  problem solved

---
+++ add_length.py
@@ -0,0 +1,11 @@
+'''
+write a function that takes a String and returns an
+list with the length of each word added to each element .
+'''
+
+def add_length(words):
+    words_len = [[i, str(len(i))] for i in words.split()]
+    return [' '.join(i) for i in words_len]
+    
+
+assert add_length('carrot cake') == ['carrot 6', 'cake 4'] #testcase
