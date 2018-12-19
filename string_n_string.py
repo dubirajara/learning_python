d10b7fdfce79efc4bba3fc9fb2757d0a3e02b1ff
Diego Ubirajara <diego.ubirajara@mail.com>
2018-12-19 18:42:16 +0100

    refactor pep8

--- string_n_string.py
+++ string_n_string.py
@@ -2,9 +2,11 @@
 We have a string s
 We have a number n
 
-Create a function string_n_string(s, n) that takes your string, concatenates the even-indexed chars to the front, 
-odd-indexed chars to the back and return the result of the string after applying the function to it n times.
+Create a function string_n_string(s, n) that takes your string, 
+concatenates the even-indexed chars to the front, odd-indexed chars to the back
+and return the result of the string after applying the function to it n times.
 """
+
 
 def string_n_string(s, n):
     for _ in range(n):
@@ -15,5 +17,5 @@ def string_n_string(s, n):
     return s
 
 
-assert string_n_string("Wow Example!", 1) == "WwEapeo xml!" #testcase
-assert string_n_string("qwertyuio", 2) == "qtorieuwy" #testcase
+assert string_n_string("Wow Example!", 1) == "WwEapeo xml!"  # testcase
+assert string_n_string("qwertyuio", 2) == "qtorieuwy"  # testcase
