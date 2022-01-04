57a1703df8dc235c7ec6b20b00bc4d9d8ab1e0e7
Diego Ubirajara <15200556+dubirajara@users.noreply.github.com>
2022-01-04 17:20:12 +0100

    Update add_length.py

--- add_length.py
+++ add_length.py
@@ -5,8 +5,7 @@ list with the length of each word added to each element.
 
 
 def add_length(words):
-    words_len = [[i, str(len(i))] for i in words.split()]
-    return [' '.join(i) for i in words_len]   
+    return [f'{i} {str(len(i))}' for i in words.split()]  
 
 
 assert add_length('carrot cake') == ['carrot 6', 'cake 4']  # testcase
